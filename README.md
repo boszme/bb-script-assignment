60e6a2f3fc6ed3ef2f004eebd32590bcbc321a4b
boszme <dddrue@gmail.com>
2018-04-07 19:43:56 +0200

    Initial commit Added README Added basic scraper

---
+++ README.md
@@ -0,0 +1,5 @@
+## Requirements:
+
+- python3
+- sudo apt install python3-pip
+- pip3 install lxml pyquery
---
+++ scrape_rss.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from pyquery import PyQuery as pq
+from lxml import etree
+import urllib
+import re
+
+d = pq("<html><head><link href=\"http://link.html\">link</link></head><body><a href=\"egy.html\">egy</a><a href=\"http://ketto.html\">ketto</a><a href=\"https://harom.html\">harom</a></body></html>")
+#d = pq(etree.fromstring("<html></html>"))
+
+for a in d('a,link'):
+    tmp = a.attrib['href']
+    if (re.search('https?://.*', tmp)):
+        print (tmp)
