13c523ebe4c29984884dace839f62c05121a790b
boszme <dddrue@gmail.com>
2018-04-08 18:38:58 +0200

    Added read from stdin

--- scrape_rss.py
+++ scrape_rss.py
@@ -4,19 +4,18 @@ from lxml import etree
 import urllib
 import re
 import json
+import sys
 
-urlok = {
+urls = {
     "atom": [],
     "rss": []
 }
 
-
-d = pq("<html><head><link href=\"http://link.html\">link</link></head><body><a href=\"egy.html\">egy</a><a href=\"http://ketto.html\">ketto</a><a href=\"https://harom.html\">harom</a></body></html>")
-#d = pq(etree.fromstring("<html></html>"))
+d = pq(etree.fromstring(sys.stdin.read()))
 
 for a in d('a,link'):
     tmp = a.attrib['href']
     if (re.search('https?://.*', tmp)):
-        urlok["atom"].append(tmp)
+        urls["atom"].append(tmp)
 
-print (json.dumps(urlok, indent=4))
+print (json.dumps(urls, indent=4))
