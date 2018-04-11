5235b3e983758edc31a52b98e30f29beae04eb1e
boszme <dddrue@gmail.com>
2018-04-11 09:28:02 +0200

    Added content type checker function

--- scrape_rss.py
+++ scrape_rss.py
@@ -11,11 +11,24 @@ urls = {
     "rss": []
 }
 
+
+def get_content_type(url):
+    f = urllib.request.urlopen(url)
+    content_header = (f.info().get("Content-Type"))
+    if ";" in content_header:
+        content_type, charset = content_header.split(";")
+    else:
+        content_type = content_header
+    return content_type
+
+
 d = pq(sys.stdin.read(), parser='html')
 
 for a in d('a,link'):
     tmp = a.attrib['href']
     if (re.search('https?://.*', tmp)):
         urls["atom"].append(tmp)
+
+
 
 print (json.dumps(urls, indent=4))
