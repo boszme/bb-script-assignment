01c08de4e982a6b0738233f5951aebb7336c3764
boszme <dddrue@gmail.com>
2018-04-11 11:50:31 +0200

    Added basis network error handling Implemented the separation of atom/rss feeds

--- scrape_rss.py
+++ scrape_rss.py
@@ -6,6 +6,8 @@ import re
 import json
 import sys
 
+urls_tmp = []
+
 urls = {
     "atom": [],
     "rss": []
@@ -13,12 +15,16 @@ urls = {
 
 
 def get_content_type(url):
-    f = urllib.request.urlopen(url)
-    content_header = (f.info().get("Content-Type"))
-    if ";" in content_header:
-        content_type, charset = content_header.split(";")
-    else:
-        content_type = content_header
+    try:
+        f = urllib.request.urlopen(url)
+        content_header = (f.info().get("Content-Type"))
+        if ";" in content_header:
+            content_splitted = content_header.split(";")
+            content_type = content_splitted[0]
+        else:
+            content_type = content_header
+    except:
+        content_type = "ERROR"
     return content_type
 
 
@@ -26,9 +32,15 @@ d = pq(sys.stdin.read(), parser='html')
 
 for a in d('a,link'):
     tmp = a.attrib['href']
-    if (re.search('https?://.*', tmp)):
-        urls["atom"].append(tmp)
+    if (re.search('^https?://.*', tmp)):
+        urls_tmp.append(tmp)
 
-
+for link in urls_tmp:
+    ct = get_content_type(link)
+    print ("ct url", ct, link)
+    if ct == "application/rss+xml":
+        urls["rss"].append(link)
+    elif ct == "application/atom+xml":
+        urls["atom"].append(link)
 
 print (json.dumps(urls, indent=4))
